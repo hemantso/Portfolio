81ea93cc5479034b9c222f72fddbed7ae4054142
hemant-soni-vst-au4 <sonihemant4741@gmail.com>
2021-03-04 18:12:22 +0530

    updated portfolio

---
+++ index.js
@@ -0,0 +1,368 @@
+const ProjectsContent = {
+    aoKanji: {
+      title: 'MovieNchill',
+      subtitle: ['Microverse', 'React', 2020],
+      img: './images/movienchill.png',
+      body: `<div class="modal-body"><p>
+      MovieNchill browsing Movies wep app created with React/Redux and TMDB API. 
+      </p>
+      <hr/>
+      <p><strong>MovieNchill</strong> is a Single Page Application (SPA) built with React and Redux to browse and search movies. The design of the web app resembles Netflix. The data is fetched from TMDB API.</p>
+  <p>For this project I chose a free API that serves Movie lists and details, including examples of movie's actors and description, in the app, it's possible to browse a pre-defined list of movies.</p>
+  <p><strong>Features</strong></p>
+  <ul>
+  <li>
+  <p>Loading visual feedback between data fetch requests</p>
+  </li>
+  <li>
+  <p>Fetching Movies lists from different API endpoints</p>
+  </li>
+  <li>
+  <p>"Search" <em>input</em> at the navbar to browse</p>
+  </li>
+  <li>
+  <p>Mobile-first design</p>
+  </li>
+  <li>
+  <p>Vedio trailer of movies</p>
+  </li>
+  <li>
+  <p>Centralized state for fetched Movie data</p>
+  </li>
+  <li>
+  <p>Centralized state for movie list sorting and filtering</p>
+  </li>
+  <li>
+  <p>Jest testing</p>
+  </li>
+  <li>
+  <p>Unique client-side routes</p>
+  </li>
+  <li>
+  <p>Use of React hooks</p>
+  </li>
+  <li>
+  <p>Use of CSS modules and BEM</p>
+  </li>
+  </ul>
+  </div>
+  `,
+    stack: ['React', 'Redux', 'Webpack'],
+    live: 'https://movienchill.herokuapp.com/',
+    repo: 'https://github.com/hemant-soni-vst-au4/movienchill',
+    },
+    circle: {
+      title: 'Lifestyle Articles',
+      subtitle: ['Microverse', 'Rails & Bootstrap', 2020],
+      img: './images/lifes.png',
+      body: `<div class="modal-body"><p>
+      Application where users can register using email, username, password and can sign in with their email and password. As a logged in user, it is possible to create articles, edit profile, vote an article.
+      Only Admin is allowed to create new category and other logged in user can create article through these category </p>
+      <br/>
+      <p>only logged-in user can like the articles and other user can read the articles</p>
+  
+  <p><strong>Features</strong></p>
+  <ul>
+  <li>
+  <p>
+  Material UI
+  </p>
+  </li>
+  <li>
+  <p>
+  Rails API
+  </p>
+  </li>
+  <li>
+  <p>
+  DRY methodology
+  </p>
+  </li>
+  <li>
+  <p>
+  Article filter categories wise
+  </p>
+  </li>
+  <li>
+  <p>
+  Token-based authentication and athorizations
+  </p>
+  </li>
+  <li>
+  <p>Unit and integration testing</p>
+  </li>
+  </ul>
+  </div>
+  `,
+    stack: ['Rails', 'Bootstrap', 'Unit testing'],
+    live: 'https://evening-spire-40702.herokuapp.com/',
+    repo: 'https://github.com/hemant-soni-vst-au4/Lifestyle_articles',
+    },
+    depths: {
+      title: 'RPG Game',
+      subtitle: ['Microverse', 'Phaser3', 2020],
+      img: './images/rpg.png',
+      body: `<div class="modal-body"><p>
+      A Phaser3 game is based on the concept that a player will help monkey to eat bananas in the forest by escaping from the Wild Cats.
+      <br/>
+      <p>
+      In this project, I implemented a game with wild cat coming  towards the monkey, monkey's jump movement, hanging banana, eating bananas, with a forest theme.</p>
+      <p><strong>About the Game</strong></p>
+      <p>
+      From the depths consists of a RPG game with the twist that it happens in forest.
+  </p><p>
+  A monkey is looking for the bananas in a forest.  
+      </p><p>
+      But the Wild Cats are all over roaming around the forest. The player will get bananas for a monkey by helping to escape from the Wild Cats.
+      </p><p>
+      All the art feature in this game is original if not indicated otherwise at the Attributions section.
+  </p>
+  <p><strong>Features</strong></p>
+  <ul>
+  <li>
+  <p>
+  Jump stuff!</p>
+  </li>
+  <li>
+  <p>
+  Eat banana!</p>
+  </li>
+  <li>
+  <p>
+  Escaping from wild cat by jumping</p>
+  </li>
+  <li>
+  <p>
+  Battle of monkey and cat!</p>
+  </li>
+  <li>
+  <p>Point table</p>
+  </li>
+  <li>
+  <p>Check if your score at the top of the LeaderBoard!</p>
+  </li>
+  </ul>
+  </div>
+  `,
+    stack: ['Phaser3', 'JavaScript', 'Webpack'],
+    live: 'https://sleepy-bardeen-ee67a3.netlify.app/',
+    repo: 'https://github.com/hemant-soni-vst-au4/RPG-Game',
+    },
+    eggo: {
+      title: 'Lo Eggo',
+      subtitle: ['Microverse', 'JavaScript', 2020],
+      img: './images/rpg.png',
+      body: `<div class="modal-body"><p>
+      A breakfast restaurant page with client-side which allows users to toggle between the restaurant about page, menu, and contact information.
+      <br/>
+      <p><strong>About the Project</strong></p>
+      <p>
+      This project is a dynamically rendered restaurant page. Each tab('home', 'contact', 'about', 'menu') is in its own javascript file at /src and imported to index.js to be rendered when a nav link is clicked. The adding and removing HTML from the page, as well as the entering and leaving CSS animation, are handled by the page factory methods from where each page is made.
+      </p>
+  <p><strong>Features</strong></p>
+  <ul>
+  <li>
+  <p>
+  Neumorphic design</p>
+  </p>
+  </li>
+  <li>
+  <p>
+  Pure CSS animation and interactions</p>
+  </li>
+  <li>
+  <p>
+  Client side pagination</p>
+  </li>
+  </ul>
+  </div>`,
+    stack: ['JavaScript', 'Webpack'],
+    live: 'https://uryelah.github.io/Microverse-603-restaurant-page/#',
+    repo: 'https://github.com/uryelah/Microverse-603-restaurant-page',
+    },
+    tic: {
+      title: 'Tic-tac-toe',
+      subtitle: ['Ruby', 'JavaScript', 2020],
+      img: './images/tic.png',
+      body: `<div class="modal-body"><p>
+      A ruby version of the traditional Tic Tac Toe that let's two players have matches via terminal commands. 
+      <br/>
+      <p><strong>About the Game</strong></p>
+      <p>
+      Tic-tac-toe (American English), noughts and crosses (British English), or Xs and Os is a paper-and-pencil game for two players, X and O, who take turns marking the spaces in a 3×3 grid. The player who succeeds in placing three of their marks in a horizontal, vertical, or diagonal row is the winner.
+      </p>
+  <p><strong>Features</strong></p>
+  <ul>
+  <li>
+  <p>
+  100% terminal game play</p>
+  </p>
+  </li>
+  <li>
+  <p>
+  Score keeping</p>
+  </li>
+  <li>
+  <p>
+  Multiple rounds game</p>
+  </li>
+  <li>
+  <p>
+  Two player support
+  </p>
+  </li>
+  </ul>
+  </div>`,
+    stack: ['Ruby'],
+    repo: 'https://github.com/hemant-soni-vst-au4/tic-tac-toe-2',
+    },
+    flag: {
+      title: 'Weather Checker',
+      subtitle: ['Project', 'Javascript', 'Webpack', 2020],
+      img: './images/weather.png',
+      body: `<div class="modal-body"><p>
+      This project is a simple SPA weather database made with the data from REST weather API.
+      <br/>
+      <p><strong>About the Project</strong></p>
+      <p>
+      An app that tells the weather. Weather data fetched using the API.
+            It equipped with writting city name in search box and press enter. you can see searched city's weather!
+      </p>
+  <p><strong>Features</strong></p>
+  <ul>
+  <li>
+  <p>
+  See all countrie's weather from the API on the homepage</p>
+  </p>
+  </li>
+  <li>
+  <p>
+  Search for a country using an <em>input</em> field</p>
+  </li>
+  <li>
+  <p>
+  Toggle the temprature between celcius and fahrenite
+  </p>
+  </li>
+  </ul>
+  </div>`,
+    stack: ['Javascript', 'Webpack'],
+    repo: 'https://github.com/hemant-soni-vst-au4/weather-app',
+    live: 'https://youthful-neumann-3e52e4.netlify.app/',
+    },
+  };
+  
+  window.onload = () => {
+    const nav = document.getElementById('nav');
+    const dropDowns = document.getElementsByClassName('about-skill');
+    const navExpand = document.getElementById('nav-expand');
+    const navClose = document.getElementById('nav-close');
+    const navMobile = document.getElementById('nav--mobile');
+    const mobileLinks = document.getElementsByClassName('mobile-link');
+    const backBtn = document.getElementById('back');
+    const main = document.getElementById('main');
+    const items = document.getElementsByClassName('item');
+    const readMoreBtn = document.getElementsByClassName('more');
+    const modal = document.getElementById('modal');
+    const clickCatcher = document.getElementById('click-catcher');
+    const closeIco = document.getElementById('close-ico');
+  
+    const modalTitle = document.getElementById('modal-title');
+    const modalSubtitle = document.getElementById('modal-subtitle');
+    const modalImg = document.getElementById('modal-img');
+    const modalDesc = document.getElementById('modal-desc');
+    const modalStack = document.getElementById('modal-stack');
+    const modalLive = document.getElementById('modal-live');
+    const modalRepo = document.getElementById('modal-repo');
+  
+    clickCatcher.addEventListener('click', e => {
+      modal.classList.add('hide');
+      main.classList.remove('blurred');
+      clickCatcher.classList.add('hide');
+    });
+  
+    backBtn.addEventListener('click', e => {
+      modal.classList.add('hide');
+      main.classList.remove('blurred');
+      clickCatcher.classList.add('hide');
+    });
+  
+    closeIco.addEventListener('click', e => {
+      modal.classList.add('hide');
+      main.classList.remove('blurred');
+      clickCatcher.classList.add('hide');
+    });
+  
+    const openModal = () => {
+      modal.classList.remove('hide');
+      clickCatcher.classList.remove('hide');
+      main.classList.add('blurred');
+    };
+  
+    window.document.addEventListener('scroll', e => {
+      if (e.target.scrollingElement.scrollTop > 35) {
+        nav.classList.add('shadow-1');
+      } else {
+        nav.classList.remove('shadow-1');
+      }
+    });
+    document.querySelector('#null').checked = true;
+  
+    navExpand.addEventListener('click', () => {
+      navMobile.classList.remove('hide');
+      main.classList.add('blurred');
+    });
+  
+    navClose.addEventListener('click', () => {
+      navMobile.classList.add('hide');
+      main.classList.remove('blurred');
+    });
+  
+    Array.from(mobileLinks).forEach(link => {
+      link.addEventListener('click', () => {
+        navMobile.classList.add('hide');
+        main.classList.remove('blurred');
+      });
+    });
+  
+    Array.from(readMoreBtn).forEach(item => {
+      item.addEventListener('click', () => {
+        // change content of modal
+        modalTitle.innerText = ProjectsContent[item.dataset.item].title;
+        Array.from(modalSubtitle.children).forEach((sub, i) => {
+          sub.innerText = ProjectsContent[item.dataset.item].subtitle[i];
+        });
+        modalImg.src = ProjectsContent[item.dataset.item].img;
+        modalDesc.innerHTML = ProjectsContent[item.dataset.item].body;
+        Array.from(modalStack.children).forEach((sub, i) => {
+          sub.classList.remove('hide');
+  
+          if (!ProjectsContent[item.dataset.item].stack[i]) {
+            sub.classList.add('hide');
+          } else {
+            sub.innerText = ProjectsContent[item.dataset.item].stack[i];
+          }
+        });
+  
+        if (ProjectsContent[item.dataset.item].live) {
+          modalLive.classList.remove('hide');
+          modalLive.href = ProjectsContent[item.dataset.item].live;
+        } else {
+          modalLive.classList.add('hide');
+        }
+        modalRepo.href = ProjectsContent[item.dataset.item].repo;
+        openModal();
+      });
+    });
+  
+    Array.from(dropDowns).forEach(drop => {
+      drop.addEventListener('click', () => {
+        let dropInput = document.getElementById(drop.attributes.for.nodeValue);
+        if (dropInput.checked) {
+          setTimeout(() => {
+            document.querySelector('#null').click();
+          })
+        }
+      });
+    });
+  };
